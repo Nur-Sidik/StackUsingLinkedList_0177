bd593ee2a8f7c3abf88940d24ae12fbb1932f2ac
Nur-Sidik <nursidik1500@gmail.com>
2025-05-22 11:03:44 +0700

    menambahkan logika untuk operasi pop pada stack

--- Stack.cpp
+++ Stack.cpp
@@ -95,7 +95,19 @@ int main()
                 cin >> value;
                 stack.push(value);
                 break;
-    
+            case 2:
+                if (!stack.isEmpty())
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    cout << "Stack Is empty. Cannot pop." << endl;
+                }
+            break;
+
+        }
+        cout << endl;
     }
     return 0;
 }
